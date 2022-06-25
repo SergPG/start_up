eba13389ac42c6e07862f54589b49aacda11bddb
SergPG <serg-popov@ukr.net>
2022-06-26 00:47:31 +0300

    Fix

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :controller do
+
+    describe 'POST #create' do
+        let(:params) do
+            {
+                article: {
+                    title: 'Title Test RSpec',
+                    body:   'Full texst in body Test',
+                    status: 'public'
+              }
+            }
+          end
+
+        # it 'saves the new article in the database' do
+        #     expect { post :create, params: }.to eq(article:)
+        # end  
+
+        it 'returns 302 status' do
+            post :create, params: params
+            expect(response).to have_http_status(302)
+          end
+
+    end    
+
+end    
